9ab150961276004eedc3f34726ca6272ce9a3646
Larry May <larrymayster@gmail.com>
2023-01-05 20:52:35 -0500

    Initial commit
    Working on an initial ESXi format module for nix, based on a github
    comment:
    https://github.com/NixOS/nixpkgs/issues/94104#issuecomment-1168929731

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "A very basic flake";
+
+  inputs.nixos-generators.url = "github:mayl/nixos-generators/custom_formats";
+
+  outputs = { self, nixpkgs, nixos-generators, ... }: {
+
+    customFormats = {
+      "esxi" = { formatAttr = "esxi"; imports = [ ./esxi_image.nix ]; };
+    };
+
+    packages.x86_64-linux.testEsxi = nixos-generators.nixosGenerate {
+      format = "esxi";
+      system = "x86_64-linux";
+      customFormats = self.customFormats;
+      modules = [
+        ({pkgs, ...}: {
+          environment.systemPackages = with pkgs; [ cowsay ];
+        })
+      ];
+    };
+
+    packages.x86_64-linux.testVM = nixos-generators.nixosGenerate {
+      format = "vm";
+      system = "x86_64-linux";
+      customFormats = self.customFormats;
+      modules = [
+        ({pkgs, ...}: {
+          environment.systemPackages = with pkgs; [ cowsay ];
+        })
+      ];
+    };
+
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+
+  };
+}
